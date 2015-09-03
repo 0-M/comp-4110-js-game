5d7fb3565b03e325d9e41c570b58119da928e529
Zachary Krausman <zkrausman@gmail.com>
2015-09-03 18:10:07 -0400

    updated namnees

--- Semester1/Assignment11/typedef.h
+++
@@ -1,9 +0,0 @@
-/*****************************************************/
-/* Programmer: S. Bassil (Fall 2014)                 */
-/*                                                   */
-/* Program: assignment11                             */
-/*                                                   */
-/* Approximate Completion Time:                      */
-/*****************************************************/
-
-typedef unsigned char byte;
