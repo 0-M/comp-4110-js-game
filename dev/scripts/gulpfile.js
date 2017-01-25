9176b0f66be65d94c99ce867cacfcbff87b81704
mmarkman <mmarkman@users.noreply.github.com>
2017-01-25 10:39:10 -0500

    not working yet
    it’s not supposed to be

---
+++ dev/scripts/gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './main.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('../../public/'));
+});
+
+
+
+gulp.task('watch', function(){
+  gulp.watch("./*.js", ['javascript'])
+  .on('end', function(){ gutil.log('Done! Watching...'); })
+});
+
+
+gulp.task('default', ['watch']);
