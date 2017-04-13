b4c0d43522a44099845c819638add4a3c876e169
mmarkman <mmarkman@users.noreply.github.com>
2017-04-13 17:50:12 -0400

    he follows you

--- src/sprites/ClownBoss.js
+++ src/sprites/ClownBoss.js
@@ -5,9 +5,9 @@ import { getBlockableMap } from '../utils'
 export class ClownBoss extends Phaser.Sprite {
   constructor (game, asset, tileX, tileY, difficulty) {
     super(game, tileX * game.tileWidth, tileY * game.tileWidth, 'clownBoss')
+
     game.add.existing(this)
-    this.tileX = tileX
-    this.tileY = tileY
+    game.physics.arcade.enable(this)
 
     this.difficulty = difficulty
     this.health = 100 * difficulty
@@ -20,23 +20,52 @@ export class ClownBoss extends Phaser.Sprite {
     var binaryMap = getBlockableMap(coll)
     var grid = new PF.Grid(binaryMap)
     this.grid = grid.clone()
-    this.finder = new PF.AStarFinder()
+    this.finder = new PF.AStarFinder({
+      allowDiagonal: true,
+      dontCrossCorners: true
+    })
+    var myX = Math.round(this.x / this.game.tileWidth)
+    var myY = Math.round(this.y / this.game.tileWidth)
     var playerTileX = Math.round(this.game.player.x / this.game.tileWidth)
     var playerTileY = Math.round(this.game.player.y / this.game.tileWidth)
-    console.log('path to player: ' + this.finder.findPath(this.tileX, this.tileY, playerTileX, playerTileY, grid))
+    console.log('path to player: ' + this.finder.findPath(myX, myY, playerTileX, playerTileY, grid))
   }
 
   findPath () {
     var grid = this.grid
     this.grid = grid.clone()
+    var myX = Math.round(this.x / this.game.tileWidth)
+    var myY = Math.round(this.y / this.game.tileWidth)
     var playerTileX = Math.round(this.game.player.x / this.game.tileWidth)
     var playerTileY = Math.round(this.game.player.y / this.game.tileWidth)
-    var path = this.finder.findPath(this.tileX, this.tileY, playerTileX, playerTileY, grid)
+    var path = this.finder.findPath(myX, myY, playerTileX, playerTileY, grid)
     console.log('path to player: ' + path)
+//    path.pop()
+    this.moveUsingPath(myX, myY, path)
   }
 
-  moveUsingPath (path) {
+  moveUsingPath (myX, myY, path) {
+    this.body.velocity.x = 0
+    this.body.velocity.y = 0
 
+    if (path.length > 1) {
+      let next = {x: path[1][0], y: path[1][1]}
+      let dx = myX - next.x
+      let dy = myY - next.y
+
+      if (dx > 0) {
+        this.body.velocity.x = -150
+      }
+      if (dx < 0) {
+        this.body.velocity.x = 150
+      }
+      if (dy > 0) {
+        this.body.velocity.y = -150
+      }
+      if (dy < 0) {
+        this.body.velocity.y = 150
+      }
+    }
   }
 
   update () {
