4c3a02c116609f62ed2e1bb5d189d5c2728722f6
Victor Espailat <vicander_99@hotmail.com>
2017-04-20 21:50:15 -0400

    Implemented Enemy movement animation. Added attack animations to fat_clown spritesheet

--- src/sprites/Enemy.js
+++ src/sprites/Enemy.js
@@ -18,6 +18,45 @@ export class Enemy extends Phaser.Sprite {
     this.meleeCooldown = 0
     this.attacking = false
     this.setupPathFinding()
+
+    this.animating = false
+
+    this.walkSpeed = 150
+    this.walkAnimSpeed = 6    // Frames per second
+
+    this.attackAnimSpeed = 15 // Frames per second
+    this.attackDuration = 1000 // Milliseconds
+
+    this.lastAnimation = 'down'
+    this.animations.add('upwalk', [12, 13, 14, 15])
+    this.animations.add('leftwalk', [4, 5, 6, 7])
+    this.animations.add('rightwalk', [8, 9, 10, 11])
+    this.animations.add('downwalk', [0, 1, 2, 3])
+
+    this.animations.add('attack', [16,17,18,19,20,21,22,23,24,25,26])
+  }
+
+  animateWalkingUp () {
+    this.animations.play('upwalk', this.walkAnimSpeed, false)
+  }
+  animateWalkingDown () {
+    this.animations.play('downwalk', this.walkAnimSpeed, false)
+  }
+  animateWalkingLeft () {
+    this.animations.play('leftwalk', this.walkAnimSpeed, false)
+  }
+  animateWalkingRight () {
+    this.animations.play('rightwalk', this.walkAnimSpeed, false)
+  }
+
+  animateAttacking () {
+    this.animations.play('attack', this.attackAnimSpeed, false)
+    this.lastAnimation = 'down'
+    setTimeout(() => {this.setAnimatingFalse()}, this.attackDuration);
+  }
+
+  setAnimatingFalse() {
+    this.animating = false
   }
 
   cooldown (key) {
@@ -64,22 +103,42 @@ export class Enemy extends Phaser.Sprite {
     this.body.velocity.x = 0
     this.body.velocity.y = 0
 
-    if (path.length > 1) {
-      let next = {x: path[1][0], y: path[1][1]}
-      let dx = myX - next.x
-      let dy = myY - next.y
+    if(!this.animating) {
+      if (path.length > 1) {
+        let next = {x: path[1][0], y: path[1][1]}
+        let dx = myX - next.x
+        let dy = myY - next.y
 
-      if (dx > 0) {
-        this.body.velocity.x = -150
-      }
-      if (dx < 0) {
-        this.body.velocity.x = 150
-      }
-      if (dy > 0) {
-        this.body.velocity.y = -150
-      }
-      if (dy < 0) {
-        this.body.velocity.y = 150
+        if (dx > 0) {
+          this.body.velocity.x = -150
+          this.animateWalkingLeft()
+          this.lastAnimation = 'left'
+        }
+        if (dx < 0) {
+          this.body.velocity.x = 150
+          this.animateWalkingRight()
+          this.lastAnimation = 'right'
+        }
+        if (dy > 0) {
+          this.body.velocity.y = -150
+          this.animateWalkingUp()
+          this.lastAnimation = 'up'
+        }
+        if (dy < 0) {
+          this.body.velocity.y = 150
+          this.animateWalkingDown()
+          this.lastAnimation = 'down'
+        }
+      } else {
+        if (this.lastAnimation == 'up') {
+          this.frame = 12
+        } else if (this.lastAnimation == 'down') {
+          this.frame = 0
+        } else if (this.lastAnimation == 'left') {
+          this.frame = 4
+        } else {
+          this.frame = 8
+        }
       }
     }
   }
@@ -107,6 +166,10 @@ export class Enemy extends Phaser.Sprite {
       this.attacking = true
       if (this.meleeCooldown === 0 && this.closeEnoughToPlayerToMelee()) {
         // do melee attack
+
+        this.animating = true
+        this.animateAttacking()
+
         combat.handleAttackOnPlayer(this.game.player, this)
         this.meleeCooldown = this.meleeTime
         this.cooldown('meleeCooldown')
