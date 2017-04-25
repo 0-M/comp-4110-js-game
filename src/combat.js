38cf6c456e17677f5357493590aba701e23127b6
mmarkman <mmarkman@users.noreply.github.com>
2017-04-25 18:32:32 -0400

    attacks bruh

--- src/combat.js
+++ src/combat.js
@@ -37,61 +37,71 @@ class Combat {
   attackLeft (player) {
     var currentState = player.game.state.current
     var enemies = player.game.state.states[currentState].enemies
-    enemies.forEach((enemy) => {
-      var dx = player.x - enemy.x
-      var dy = Math.abs(enemy.y - player.y)
-      if ((dy < 50) && (dx >= 0) && (dx < 50)) {
-        this.handleAttackOnNPC(player, enemy)
-      }
-    })
+    if (enemies) {
+      enemies.forEach((enemy) => {
+        var dx = player.x - enemy.x
+        var dy = Math.abs(enemy.y - player.y)
+        if ((dy < 50) && (dx >= 0) && (dx < 50)) {
+          this.handleAttackOnNPC(player, enemy)
+        }
+      })
+    }
   }
 
   attackRight (player) {
     var currentState = player.game.state.current
     var enemies = player.game.state.states[currentState].enemies
-    enemies.forEach((enemy) => {
-      var dx = enemy.x - player.x
-      var dy = Math.abs(enemy.y - player.y)
-      if ((dy < 50) && (dx >= 0) && (dx < 50)) {
-        this.handleAttackOnNPC(player, enemy)
-      }
-    })
+    if (enemies) {
+      enemies.forEach((enemy) => {
+        var dx = enemy.x - player.x
+        var dy = Math.abs(enemy.y - player.y)
+        if ((dy < 50) && (dx >= 0) && (dx < 50)) {
+          this.handleAttackOnNPC(player, enemy)
+        }
+      })
+    }
   }
 
   attackUp (player) {
     var currentState = player.game.state.current
     var enemies = player.game.state.states[currentState].enemies
-    enemies.forEach((enemy) => {
-      var dx = Math.abs(player.x - enemy.x)
-      var dy = player.y - enemy.y
-      if ((dx < 50) && (dy >= 0) && (dy < 50)) {
-        this.handleAttackOnNPC(player, enemy)
-      }
-    })
+    if (enemies) {
+      enemies.forEach((enemy) => {
+        var dx = Math.abs(player.x - enemy.x)
+        var dy = player.y - enemy.y
+        if ((dx < 50) && (dy >= 0) && (dy < 50)) {
+          this.handleAttackOnNPC(player, enemy)
+        }
+      })
+    }
   }
 
   attackDown (player) {
     var currentState = player.game.state.current
     var enemies = player.game.state.states[currentState].enemies
-    enemies.forEach((enemy) => {
-      var dx = Math.abs(player.x - enemy.x)
-      var dy = enemy.y - player.y
-      if ((dx < 50) && (dy >= 0) && (dy < 50)) {
-        this.handleAttackOnNPC(player, enemy)
-      }
-    })
+    if (enemies) {
+      enemies.forEach((enemy) => {
+        var dx = Math.abs(player.x - enemy.x)
+        var dy = enemy.y - player.y
+        if ((dx < 50) && (dy >= 0) && (dy < 50)) {
+          this.handleAttackOnNPC(player, enemy)
+        }
+      })
+    }
   }
 
   attackAround (player) {
     var currentState = player.game.state.current
     var enemies = player.game.state.states[currentState].enemies
-    enemies.forEach((enemy) => {
-      var dx = Math.abs(player.x - enemy.x)
-      var dy = Math.abs(enemy.y - player.y)
-      if ((dx < 50) && (dy < 50)) {
-        this.handleAttackOnNPC(player, enemy)
-      }
-    })
+    if (enemies) {
+      enemies.forEach((enemy) => {
+        var dx = Math.abs(player.x - enemy.x)
+        var dy = Math.abs(enemy.y - player.y)
+        if ((dx < 50) && (dy < 50)) {
+          this.handleAttackOnNPC(player, enemy)
+        }
+      })
+    }
   }
 }
 
