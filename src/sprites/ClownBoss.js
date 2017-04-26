f243ff46415b1c28420f9571147a76799725352e
Victor Espailat <vicander_99@hotmail.com>
2017-04-25 21:43:22 -0400

    bossClown dying sound.

--- src/sprites/ClownBoss.js
+++ src/sprites/ClownBoss.js
@@ -17,6 +17,8 @@ export class ClownBoss extends Enemy {
     this.animations.add('attack', [16,16,16,17,17,17,18,19,20,21,22,23,24,25,16])
     this.animations.add('die', [0,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77])
 
+    this.killedSound = game.add.audio('clownBoss_death_sound')
+    this.killedSound.volume = 2.0
     this.attackSound = game.add.audio('claw_slash')
     this.attackSound.volume = 0.1
 
